4c9031e9f2879ce2f0329fbe027db131166e44ed
unmanbearpig <unmanbearpig@gmail.com>
2015-03-22 22:12:47 +0300

    fixed error duh

--- test/support/acceptance_test.rb
+++ test/support/acceptance_test.rb
@@ -1,25 +1,27 @@
 require_relative 'html_helper'
 
-class AcceptanceTest < Minitest::Test
-  include HTMLHelper
+module CSClient
+  class AcceptanceTest < Minitest::Test
+    include HTMLHelper
 
-  def client
-    @client ||= Client.new username: username, password: password
-  end
+    def client
+      @client ||= Client.new username: username, password: password
+    end
 
-  def username
-    ENV['CS_USERNAME']
-  end
+    def username
+      ENV['CS_USERNAME']
+    end
 
-  def password
-    ENV['CS_PASSWORD']
-  end
+    def password
+      ENV['CS_PASSWORD']
+    end
 
-  def vcr(name = nil, &block)
-    VCR.use_cassette(vcr_cassette_path(name), &block)
-  end
+    def vcr(name = nil, &block)
+      VCR.use_cassette(vcr_cassette_path(name), &block)
+    end
 
-  def vcr_cassette_path name = nil
-    name ? "#{self.class.name}##{name}" : location
+    def vcr_cassette_path name = nil
+      name ? "#{self.class.name}##{name}" : location
+    end
   end
 end
