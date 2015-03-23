87427f5850d3bc9a4f79dbd1330c248244719bed
unmanbearpig <unmanbearpig@gmail.com>
2015-03-23 19:43:25 +0300

    Allow to use response object in HTMLParser.new

--- lib/cs_client/html_parser.rb
+++ lib/cs_client/html_parser.rb
@@ -10,7 +10,17 @@ module CSClient
 
         return @body_or_element
       end
-      @doc ||= Nokogiri::HTML(@body_or_element)
+      @doc ||= Nokogiri::HTML(extract_response_body(@body_or_element))
+    end
+
+    private
+
+    def extract_response_body response_or_string
+      if response_or_string.respond_to?(:body)
+        return response_or_string.body
+      else
+        response_or_string
+      end
     end
   end
 end
